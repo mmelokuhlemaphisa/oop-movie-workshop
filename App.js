314aa7f4e25d16b615d83a3c6f96eb85184ec75f
Melokuhle <melokuhlemaphisa99@gmail.com>
2025-08-15 12:20:44 +0200

    working with loops in App.js file

---
+++ App.js
@@ -0,0 +1,65 @@
+// let sentence = "Hellow world, I am Melo"
+// console.log(sentence[3]);
+
+// let listbook = ["book1", "book2", "book3"];
+
+// for (let i = 0; i < 6; i++) {
+//   if (listbook[i]) {
+//     console.log(listbook[i][0]); // Print the first character
+//   } else {
+//     console.log("undefined");
+//   }
+// }
+
+// let i=0;
+// for ( ;i<6; ){
+// i++
+// console.log(sentence[i]);
+// }
+
+// let listBook =["book1", "book2", "book3"];
+// for (let i = 0; i< listBook.length; i++ ){
+// console.log(listBook[i][0]);
+// }
+
+// let listBook = ["book1", "book2", "book3"];
+// for (x of listBook) {
+//   console.log(x);
+// }
+
+// let sentence = "Hellow world, I am Melo";
+// for (x of sentence) console.log(x);
+
+// let marks ={
+//     "zama": 34,
+//     "fanale" :89,
+//     "gabe" : 45,
+//     "safe" : 67,
+//     "kane" : 21
+// };
+
+// for (key in marks){
+
+//     console.log(key);
+// }
+
+let marks = {
+  zama: 34,
+  fanale: 89,
+  gabe: 45,
+  safe: 67,
+  kane: 21,
+};
+
+for (key in marks) {
+//   console.log(marks[key]);
+}
+
+
+let i =0;
+while(i<6){
+ console.log(i)
+ i++;
+}
+
+let condition =true;
---
+++ movies.js
@@ -0,0 +1,11 @@
+//import
+const movies = [
+ {
+    "title": "Movie Title 1",
+    "director": "Director Name",
+    "year": 2025,
+    "genre": "Drama",
+    "rating": 7.5,
+    "actors": ["steven", "jane"]
+  },
+]
